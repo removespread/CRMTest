14188c5f17ee64bd92b09937f3bb15536e79c000
Viktor S. <removespread@proton.me>
2024-11-22 23:55:27 +0300

    Added auth

--- internal/repository/handlers/auth.go
+++ internal/repository/handlers/auth.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"context"
 	"crm/internal/domain"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthHandlerInterface interface {
@@ -19,9 +21,32 @@ func NewAuthHandler(accountHandler *AccountHandler) *AuthHandler {
 }
 
 func (h *AuthHandler) Register(ctx context.Context, account *domain.RegisterAccount) error {
+	account = &domain.RegisterAccount{
+		Email:    account.Email,
+		Password: account.Password,
+	}
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(account.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	account.Password = string(passwordHash)
+
+	return h.accountHandler.Create(ctx, account)
 
 }
 
 func (h *AuthHandler) Login(ctx context.Context, account *domain.AccountLoginWithEmail) error {
+	findAccount, err := h.accountHandler.GetByEmail(ctx, account.Email)
+	if err != nil {
+		return err
+	}
 
+	err = bcrypt.CompareHashAndPassword([]byte(findAccount.Password), []byte(account.Password))
+	if err != nil {
+		return err
+	}
+
+	return h.accountHandler.LoginAccount(ctx, findAccount)
 }
