c468aafc881997da55211e57245ab4c1948f7efa
Viktor S. <removespread@proton.me>
2024-11-29 02:02:17 +0300

    fx application

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"crm/internal/repository/handlers"
+	"crm/internal/service"
 	"crm/internal/srvenv"
+	"log"
 	"net/http"
+	"os"
 
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxevent"
@@ -11,7 +15,8 @@ import (
 )
 
 func main() {
-	fx.New(
+	// fx application
+	app := fx.New(
 		fx.WithLogger(func(logger *zap.Logger) fxevent.Logger {
 			return &fxevent.ZapLogger{Logger: logger}
 		}),
@@ -23,7 +28,25 @@ func main() {
 			handlers.NewBidHandler,
 			handlers.NewPartnerHandler,
 			srvenv.NewLogger,
+			service.NewAccountService,
+			service.NewContactService,
+			service.NewBidService,
+			service.NewPartnerService,
 		),
 		fx.Invoke(func(*http.Server) {}),
-	).Run()
+	)
+
+	// if failed to start application
+	if err := app.Start(context.Background()); err != nil {
+		log.Printf("Failed to start application: %v\n", err)
+		os.Exit(1)
+	}
+
+	<-app.Done()
+
+	// if failed to stop application gracefully
+	if err := app.Stop(context.Background()); err != nil {
+		log.Printf("Failed to stop application gracefully: %v\n", err)
+		os.Exit(1)
+	}
 }
